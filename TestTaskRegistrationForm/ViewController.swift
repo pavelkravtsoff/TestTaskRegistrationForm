1fa41f6e5e30f557091a52623b587544832b3283
Паша <jigsaw@macbook-jigsaw.local>
2018-07-01 22:39:05 +0300

    Initial Commit

---
+++ TestTaskRegistrationForm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestTaskRegistrationForm
+//
+//  Created by JigSaw on 01.07.2018.
+//  Copyright © 2018 poulkravtsoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
